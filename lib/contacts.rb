73ba737243755fd6a09636c2f79b3f438c9125be
Sara McCombs <thesaramccombs@gmail.com>
2019-08-25 23:04:37 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |name, info|
+    if name == "Freddy Mercury"
+      info.each do |attribute, value|
+        if attribute == :favorite_ice_cream_flavors
+          value.delete_if {|flavor| flavor == "strawberry"}
+        end
+      end
+    end
+  end
 end
